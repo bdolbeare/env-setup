e104ad7c60da723a0c74e716327d9bad102428fe
Brian Dolbeare <brian@macbook.lan>
2018-07-01 09:22:23 -0400

    some updates on common install stuff

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+brew cask install spectacle
+brew cask install virtualbox
+brew cask install vagrant
+
+# get yarn as an alternative to npm
+brew install yarn
+
+# get a modern version of gradle
+brew update gradle
+
+
 # most is a better pager as it provides styling (e.g. run 'man ls' with most set as your pager)
 brew install most
 
